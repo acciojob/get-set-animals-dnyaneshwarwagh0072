f1afd52a604baaaa54a10cb0def0780e7838f7cc
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-10-27 19:43:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-//complete this code
-class Animal {}
+class Animal {
+    constructor(species) {
+        this._species = species;
+    }
 
-class Dog extends Animal {}
+    get species() {
+        return this._species;
+    }
 
-class Cat extends Animal {}
+    makeSound() {
+        console.log("The animal makes a sound");
+    }
+}
 
-// Do not change the code below this line
+class Cat extends Animal {
+    constructor() {
+        super("Cat");
+    }
+
+    purr() {
+        console.log("purr");
+    }
+}
+
+class Dog extends Animal {
+    constructor() {
+        super("Dog");
+    }
+
+    bark() {
+        console.log("woof");
+    }
+}
+
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
+const myCat = new Cat();
+console.log(myCat.species); 
+myCat.makeSound(); 
+myCat.purr(); 
+
+const myDog = new Dog();
+console.log(myDog.species); 
+myDog.makeSound(); 
+myDog.bark(); 
